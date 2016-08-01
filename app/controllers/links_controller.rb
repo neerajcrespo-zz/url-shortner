c4528a533fed4c777a7e1eae522cd7d2a3d8e456
neerajcrespo <neerajcrespo@gmail.com>
2016-08-01 14:31:15 +0530

    small bug fix

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,6 +3,9 @@ class LinksController < ApplicationController
 
   def go
     @link = Link.find_by_in_url!(params[:in_url])
+    if @link.out_url.first(4) != "http"
+      @link.out_url = "http://" + @link.out_url
+    end
     redirect_to @link.out_url, :status => @link.http_status
   end
   # GET /links
